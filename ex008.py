7c879d32ba89142b0229b21dd2befb3ccdad11a7
Cláudio Junior <cpratesjunior@bol.com.br>
2023-08-14 11:02:30 -0300

    Exercicios de Python

---
+++ ex008.py
@@ -0,0 +1,8 @@
+metros = float(input('Digite os metros aqui: '))
+cm = metros*100
+mm = metros*1000
+dm = metros*10
+hm = metros*8
+print('a quantidade  de metros em centimetros equivale a: {:.2f} , '
+'\ne  quantidade de metros em milimetros equivale a: {:.2f}'.format(cm , mm))
+
