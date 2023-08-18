4ccff82d18f4d2d140943414d0bfd5ad0c826f22
Cláudio Junior <cpratesjunior@bol.com.br>
2023-08-18 14:30:28 -0300

    Exercicios de Python

---
+++ ex010.py
@@ -0,0 +1,7 @@
+valor_reais = float(input('Digite o valor em Reais: '))
+
+taxa_cambio = 4.9962
+valor_dolar = valor_reais / taxa_cambio
+
+print("O valor equivalente em Dólar é: {:.2f}".format(valor_dolar))
+
