26623d2d0381c7628793ffb7ab8773697e8d5f29
Claudio-Prates <93285417+Claudio-Prates@users.noreply.github.com>
2023-08-24 10:24:20 -0300

    Update ex015.py

--- ex015.py
+++ ex015.py
@@ -1,4 +1,4 @@
- Exercício Python 15: 
+ <strong><h1>Exercício Python 15:</h1></strong> 
 Escreva um programa que pergunte a quantidade de 
 Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. 
 Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado
